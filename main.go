e22c5a460993ec34d1db20c9015d188ac3c9c6a1
agnespravida <agnespravida27@gmail.com>
2022-11-02 18:07:46 +0700

    initial

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/ping")
+	http.ListenAndServe(":4000", r)
+}
